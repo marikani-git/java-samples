53b7d1ed3b6dbbc3a5b27dbb04b7b3b1696c2915
Marikani <mail2marikani@gmail.com>
2022-07-04 19:21:20 +0530

    exception workflow sample

---
+++ JavaSamples/src/com/marikani/basics/ExceptionHandlingTest.java
@@ -0,0 +1,40 @@
+package com.marikani.basics;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class ExceptionHandlingTest {
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+
+        try {
+            testException(-5);
+            /**
+             * This won't line execute, after the exception thrown by runtime line below won't be executed inside try block
+             */
+            testException(15);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("finally block is exdcuted");
+        }
+        System.out.println("back to normal execution flow..");
+        testException(5);
+    }
+
+    public static void testException(int value) throws FileNotFoundException, IOException {
+        if (value < 0) {
+            System.out.println("Before throwing FilNotFoundException");
+            FileNotFoundException fileNotFoundException = new FileNotFoundException("Negative value passed");
+            throw fileNotFoundException;
+        } else if (value > 10) {
+            System.out.println("Before throwing IOExeception");
+            IOException ioException = new IOException("value is greater than 10");
+            throw ioException;
+        }
+        System.out.println("No exception occcured for the value --> " + value);
+    }
+}
