00a91951ab31076c920162ff555ddb2a8897479c
Marikani <mail2marikani@gmail.com>
2022-07-19 20:11:32 +0530

    java 8 features

---
+++ JavaSamples/src/com/marikani/java8/Bank.java
@@ -0,0 +1,6 @@
+package com.marikani.java8;
+
+@FunctionalInterface
+public interface Bank {
+     public void getBankName(String bankName);
+}
---
+++ JavaSamples/src/com/marikani/java8/College.java
@@ -0,0 +1,6 @@
+package com.marikani.java8;
+
+@FunctionalInterface
+public interface College {
+    public String getCollegeName(String name);
+}
